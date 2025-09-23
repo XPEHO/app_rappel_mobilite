3abf1fa1bd4e1e9e78bc48ae0d18e8fddbf2a8a9
Théo Lebègue <theo.lebegue@icloud.com>
2025-09-23 10:23:21 +0200

    chore(initPinia): initialize pinia and components in project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-// Home view
+import CounterComponent from '@/components/counterComponent.vue';
 </script>
 
 <template>
   <main>
     <h1>Accueil</h1>
     <p>Bienvenue sur l'application de rappel mobilité</p>
+
+    <CounterComponent />
   </main>
 </template>
