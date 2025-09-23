fec3153b825258bb9db84f7538f6c50347c417bc
nathanvandeperre <nathanvandeperre@gmail.com>
2025-09-23 14:41:04 +0200

    feat(splashscreen): Add basic splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.xpeho.app.rappel',
   appName: 'app_rappel_mobilite',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // Duration of the splash (2000 = 2s)
+      launchAutoHide: true,     // Automatically hide the splash after the delay
+      backgroundColor: '#E8DFDF',
+      androidSplashResourceName: 'splash.png', // Android image name
+      iosSplashResourceName: 'Default',    // IOS image name
+    },
+  },
 };
 
 export default config;
