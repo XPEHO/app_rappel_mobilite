cc1be34f9be71d88323b0a9051a0e40e789ea575
NayMak-Adeo <makusa.nayden@gmail.com>
2025-10-09 14:27:56 +0200

    remove yaki.ui

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import "@yaki_ui/yaki_ui_web_components/css/yaki_ui.css";
 
 const app = createApp(App)
 
