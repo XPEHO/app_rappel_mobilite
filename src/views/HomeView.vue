6af2ad19ce1fa6db01e10fbbb31a4d6e7af0383b
Nyra Sama <83142634+NyraSama@users.noreply.github.com>
2025-10-09 18:13:39 +0200

    feat(empty_list): Improve empty list display and provide test reminders (#21)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,6 +32,27 @@ function addReminder() {
   };
 }
 
+// TODO: Remove these functions when not needed anymore
+function addTestReminders() {
+  reminderStore.addReminder("Faire les courses", "2025-10-09T09:47", "none");
+  reminderStore.addReminder("Faire les course 2", "2025-10-09T18:30", "none");
+  reminderStore.addReminder("Réunion avec l'équipe", "2025-10-10T10:30", "none");
+  reminderStore.addReminder("Réunion avec l'équipe 2", "2025-10-10T14:30", "none");
+  reminderStore.addReminder("Tondre la pelouse", "2025-10-13T09:55", "none");
+  reminderStore.addReminder("Tondre la pelouse 2", "2025-10-13T13:51", "none");
+  reminderStore.addReminder("Appeler le plombier", "2025-10-16T16:00", "none");
+  reminderStore.addReminder("Appeler le plombier 2", "2025-10-16T16:30", "none");
+  reminderStore.addReminder("Dentiste", "2025-10-20T11:15", "none");
+  reminderStore.addReminder("Dentiste 2", "2025-10-20T15:45", "none");
+}
+
+// TODO: Remove this function when not needed anymore
+function cleanReminders() {
+  reminderStore.reminders.forEach((reminder) => {
+    reminderStore.deleteReminder(reminder.id);
+  });
+}
+
 onMounted(async () => {
   // Request permissions on app start
   reminderStore.init();
@@ -43,21 +64,36 @@ onMounted(async () => {
   <div class="page-content">
     <h1>Remembrall</h1>
 
+    <!--TODO: Remove this button when not needed anymore-->
+    <app-button :text="'Clean tasks'" @click="cleanReminders" />
+
     <div class="task-empty" v-if="Object.keys(reminderStore.remindersByDate).length === 0">
       <font-awesome-icon :icon="faCheckCircle" style="color: var(--accent-color)" />
+      <p>
+        <span>Félicitations !</span><br />
+        Aucune tâche pour le moment<br />
+        Cliquez sur le bouton ci-dessous pour créer une nouvelle tâche
+      </p>
+      <!--TODO: Remove this button when not needed anymore-->
+      <app-button :text="'Add test tasks'" @click="addTestReminders" />
     </div>
 
     <div class="task-section" :key="key" v-for="(reminders, key) in reminderStore.remindersByDate">
       <h2>{{ key }}</h2>
       <div class="task-list">
-        <task-tile v-for="reminder in reminders" :key="reminder.id" :id="reminder.id" :title="reminder.title"
-          :date="reminder.getTimeString()" />
+        <task-tile
+          v-for="reminder in reminders"
+          :key="reminder.id"
+          :id="reminder.id"
+          :title="reminder.title"
+          :date="reminder.getTimeString()"
+        />
       </div>
     </div>
   </div>
 
   <div class="bottom-action-bar">
-    <app-button :text="'Create new task'" @click="addReminder()" />
+    <app-button :text="'Create new task'" @click="addReminder" />
   </div>
 </template>
 
@@ -65,6 +101,11 @@ onMounted(async () => {
 .page-content {
   padding-bottom: 100px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: var(--section-gap);
 }
 
 h1 {
@@ -111,8 +152,14 @@ h2 {
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 200px;
-  color: var(--color-background-soft);
+  height: 50vh;
+  text-align: center;
+  gap: var(--element-gap);
+}
+
+.task-empty span {
+  font-weight: var(--font-weight-bold);
+  font-size: var(--font-size-large);
 }
 
 .task-empty svg {
